511d1356136eb7b2d7021f35be617be412aaf1bd
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-03-11 14:56:09 +0800

    Started signup page, creating a form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //Components
 import SignUp from "./components/SignUp";
+import "./App.css";
 
 function App() {
   return (
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 
+//Styling files
+import "./SignUp.css";
+
 export default class SignUp extends Component {
   render() {
-    return <div>SignUp</div>;
+    return (
+      <div>
+        <form className="signUpForm">
+          <p className="formTitle">Sign Up</p>
+        </form>
+      </div>
+    );
   }
 }
