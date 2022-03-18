0253a97d0da036997caf1b4e5c361fe6cc478af8
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-03-18 15:17:49 +0800

    Wrote many media querries to handle form responsivness

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,9 +6,16 @@ import "./SignUp.css";
 export default class SignUp extends Component {
   render() {
     return (
-      <div>
-        <form className="signUpForm">
+      <div className="signUpForm">
+        <form>
           <p className="formTitle">Sign Up</p>
+          <div style={{ "padding-bottom": "3%" }}>
+            <input className="emailInput" placeholder="Email"></input>
+          </div>
+          <div>
+            <input className="firstNameInput" placeholder="First Name" />
+            <input className="lastNameInput" placeholder="Last Name" />
+          </div>
         </form>
       </div>
     );
