b34e3e084c3e9515959546024a327490c1e687ea
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-03-24 10:56:48 +0800

    FINISHED Sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,36 +6,46 @@ import "./SignUp.css";
 export default class SignUp extends Component {
   render() {
     return (
-      <div className="signUpForm">
-        <form>
-          <p className="formTitle">Sign Up</p>
-          <div style={{}}>
-            <input className="emailInput" placeholder="Email"></input>
-          </div>
-          <div>
-            <input
-              className="firstNameInput smallerScreen"
-              placeholder="First Name"
-            />
-            <input
-              className="lastNameInput smallerScreen"
-              placeholder="Last Name"
-            />
-          </div>
-          <div>
-            <input
-              className="firstNameInput smallerScreen"
-              placeholder="Password"
-              type="password"
-            />
-            <input
-              className="lastNameInput smallerScreen"
-              placeholder="Confirm Password"
-              type="password"
-            />
-          </div>
-          <button className='signUpButton'>Submit</button>
-        </form>
+      <div>
+        <div className="signUpForm">
+          <form>
+            <p className="formTitle">Sign Up</p>
+            <div style={{ "text-align": "center" }}>
+              <div className="mobileLogo" />
+            </div>
+            <div>
+              <input className="emailInput" placeholder="Email"></input>
+            </div>
+            <div>
+              <input
+                className="firstNameInput smallerScreen"
+                placeholder="First Name"
+              />
+              <input
+                className="lastNameInput smallerScreen"
+                placeholder="Last Name"
+              />
+            </div>
+            <div>
+              <input
+                className="firstNameInput smallerScreen"
+                placeholder="Password"
+                type="password"
+              />
+              <input
+                className="lastNameInput smallerScreen"
+                placeholder="Confirm Password"
+                type="password"
+              />
+            </div>
+            <div style={{ "text-align": "center", margin: "auto" }}>
+              <button className="signUpButton">Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className="sloganText">
+          <h1>Purrfect for cat lovers!</h1>
+        </div>
       </div>
     );
   }
