22b7b71ff0313fb2bbb43f1081046f4a7a9d0c1e
MatthewCrutcher <matthew.crutcher@hotmail.com>
2022-04-15 13:01:06 +0800

    Feed error message done - error is not scaling right with other css components tho

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,19 +4,51 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 //Styling
 import "./Feed.css";
+import ErrorLabel from "./ErrorLabel";
 
 export class Feed extends Component {
+  constructor() {
+    super();
+    this.state = {
+      post: "",
+      error: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.state.post === "") {
+      this.setState({ error: "You must enter text!" });
+    } else {
+      event.preventDefault();
+      console.log("Post created");
+      this.setState({ error: "" });
+    }
+  }
   render() {
     return (
       <div>
         <Navbar />
-        <div style={{ "text-align": "center", margin: "auto" }}>
-          <input
-            className="questionPrompt"
-            placeholder="What did your cat do today?..."
-            type="text"
-          />
-          <button className="postButton">Post</button>
+
+        <div style={{ textAlign: "center", margin: "auto" }}>
+          <form onSubmit={this.handleSubmit}>
+            {" "}
+            <input
+              className="questionPrompt"
+              placeholder="What did your cat do today?..."
+              type="text"
+              onChange={(event) => {
+                this.setState({ post: event.target.value });
+              }}
+            />
+            <button className="postButton">Post</button>
+          </form>
+
+          <div
+            className={this.state.error !== "" ? "errorLabel" : "displayNone"}
+          >
+            <ErrorLabel errorLabel={this.state.error} />
+          </div>
           <div className="userPost">
             <h4>User's Name</h4>
             <p>Users post</p>
